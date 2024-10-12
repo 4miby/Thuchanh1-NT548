1dedffef414cf347ac4b406813ec3e771e7a4625
dcamm79 <dinhcam1512@gmail.com>
2024-10-13 00:05:02 +0700

    add test for vpc

--- main.tf
+++ main.tf
@@ -38,12 +38,8 @@ module "ec2" {
   private_subnet_ids     = module.vpc.private_subnet_ids
   public_sg_id           = module.security_groups.public_sg_id
   private_sg_id          = module.security_groups.private_sg_id
-  key_name               = aws_key_pair.nhom6.key_name
+  key_name               = null
   public_instance_count  = var.public_instance_count
   private_instance_count = var.private_instance_count
 }
 
-resource "aws_key_pair" "nhom6" {
-  key_name   = "Nhom6"
-  public_key = file("C:/Users/Amiby/.ssh/id_ed25519.pub")
-}
