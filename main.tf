14b186394441a8e336691827e412781e2c9fea8d
dcamm79 <dinhcam1512@gmail.com>
2024-10-13 17:41:21 +0700

    add ssh configuration for public and private instances

--- main.tf
+++ main.tf
@@ -38,8 +38,24 @@ module "ec2" {
   private_subnet_ids     = module.vpc.private_subnet_ids
   public_sg_id           = module.security_groups.public_sg_id
   private_sg_id          = module.security_groups.private_sg_id
-  key_name               = null
+  key_name               = aws_key_pair.public_ec2.key_name
   public_instance_count  = var.public_instance_count
   private_instance_count = var.private_instance_count
 }
 
+
+resource "local_file" "public_ec2_key" {
+    filename                = "public_ec2_key.pem"
+    file_permission         = "600"
+    directory_permission    = "700"
+    content                 = tls_private_key.ssh.private_key_pem
+}
+
+resource "aws_key_pair" "public_ec2" {
+  key_name   = "public_ec2_key"
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,6 +5,19 @@ resource "aws_instance" "public" {
   subnet_id              = var.public_subnet_ids[count.index]
   vpc_security_group_ids = [var.public_sg_id]
   key_name               = var.key_name
+  user_data = <<-EOF
+                #!bin/bash
+                echo "PubkeyAcceptedKeyTypes=+ssh-rsa" >> /etc/ssh/sshd_config.d/10-insecure-rsa-keysig.conf
+                systemctl reload sshd
+                echo "${tls_private_key.ssh.private_key_pem}" >> /home/ubuntu/.ssh/id_rsa
+                chown ubuntu /home/ubuntu/.ssh/id_rsa
+                chgrp ubuntu /home/ubuntu/.ssh/id_rsa
+                chmod 600   /home/ubuntu/.ssh/id_rsa
+                echo "starting ansible install"
+                apt-add-repository ppa:ansible/ansible -y
+                apt update
+                apt install ansible -y
+                EOF
 
   tags = {
     Name = "Public EC2 ${count.index + 1}"
@@ -18,8 +31,12 @@ resource "aws_instance" "private" {
   subnet_id              = var.private_subnet_ids[count.index]
   vpc_security_group_ids = [var.private_sg_id]
   key_name               = var.key_name
-
+  
   tags = {
     Name = "Private EC2 ${count.index + 1}"
   }
+}
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
