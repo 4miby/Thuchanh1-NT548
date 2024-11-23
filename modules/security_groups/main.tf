3b6d9d0531ad5dbdc293e76618caf7f22f3fe660
dcamm79 <dinhcam1512@gmail.com>
2024-11-23 22:43:03 +0700

    fix checkov failed checks

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -17,6 +17,12 @@ resource "aws_security_group" "public" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
 
   tags = {
     Name = "Public EC2 SG"
